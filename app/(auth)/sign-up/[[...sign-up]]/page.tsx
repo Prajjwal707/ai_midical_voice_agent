281c95f72768fe90d7f917ab1f8812b2b88f32e6
Prajjwal707 <prajjwal5108008@gmail.com>
2025-08-05 02:43:52 +0530

    completed the login page

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+     <div className='flex items-ceter justify-center h-screen'> 
+  <SignUp />
+  </div>
+  )
+}
